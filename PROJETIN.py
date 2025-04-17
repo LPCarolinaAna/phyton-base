db02ff601f2327fd03024abfcd7f22028d313f90
LPCarolinaAna <anacarolinalp.13@gmail.com>
2025-04-17 20:17:49 -0300

    treinamento inicial

---
+++ PROJETIN.py
@@ -0,0 +1,8 @@
+nota1 = eval(input("digite sua primeira nota"))
+nota2 = eval(input("digite sua primeira nota2"))
+    
+media = 0.4 * nota1 + 0.6 * nota2
+if  media >= 5:
+    print("aprovado")
+else:
+    print("reprovado")
---
+++ exerciciofor.py
@@ -0,0 +1,2 @@
+for num in range(10, 100+1):
+    print(num, 'é', num **2 )
